01682eb288dace3da290a72880a8dad8d8b60498
VeronikaSevcenko <Vsladkaja95@gmail.com>
2022-02-24 01:17:46 +0300

    commit

---
+++ Exercise-number-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_number_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int sum;
+
+            Console.WriteLine("Enter the number");
+
+            int p = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= p; i++)
+            {
+                if (p > 0)
+                {
+                    sum = a;
+                    a = b;
+                    b = b + sum;
+
+                    Console.WriteLine(sum);
+                }
+                else
+                {
+                    Console.WriteLine("The number printed by you must be at least zero!");
+                }
+            }
+        }
+    }
+}
